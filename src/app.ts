f813053dfd04f449512ddf6ff69bc491215c2252
Carlos Gabriel <carlosgabrielpc@live.com>
2022-02-11 20:58:40 -0300

    feat: add swagger middleware and create user doc

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan-body';
+import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
 import 'reflect-metadata';
 
@@ -9,9 +10,13 @@ import connectDatabase from '@/config/database';
 import loggerMiddleware from './middlewares/loggerMiddleware';
 import errorHandlingMiddleware from '@/middlewares/errorHandlingMiddleware';
 
+import swaggerDocs from './swagger.json';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 morgan(app, {
   noColors: true,
