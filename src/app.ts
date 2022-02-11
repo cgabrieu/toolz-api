f5e78dba49bc1c0f04b5d669717968e17399c30b
Carlos Gabriel <carlosgabrielpc@live.com>
2022-02-11 19:12:54 -0300

    fix: logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,26 @@
 import express from 'express';
-import 'express-async-errors';
 import cors from 'cors';
+import morgan from 'morgan-body';
+import 'express-async-errors';
 import 'reflect-metadata';
 
-import connectDatabase from '@/config/database';
-import errorHandlingMiddleware from '@/middlewares/errorHandlingMiddleware';
-
 import router from '@/routes';
+import connectDatabase from '@/config/database';
+import loggerMiddleware from './middlewares/loggerMiddleware';
+import errorHandlingMiddleware from '@/middlewares/errorHandlingMiddleware';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+morgan(app, {
+  noColors: true,
+  prettify: false,
+  logReqUserAgent: false,
+  stream: {
+    write: (msg: string) => loggerMiddleware.info(msg) as any,
+  },
+});
 
 app.get('/health', (_req, res) => {
   res.send('OK!');
---
+++ src/middlewares/loggerMiddleware.ts
@@ -0,0 +1,27 @@
+import winston from 'winston';
+
+const options = {
+  console: {
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+    prettyPrint: true,
+    colorize: process.stdout.isTTY,
+  },
+};
+
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console(options.console)],
+  exitOnError: false,
+});
+
+export default {
+  log: (message: string): winston.Logger => logger.info(message),
+  info: (message: string, obj?: unknown): winston.Logger => logger.info(message, obj),
+  error: (message: string, obj?: unknown): winston.Logger => logger.error(message, obj),
+  warn: (message: string, obj?: unknown): winston.Logger => logger.warn(message, obj),
+  debug: (message: string, obj?: unknown): winston.Logger => logger.debug(message, obj),
+  silly: (message: string, obj?: unknown): winston.Logger => logger.silly(message, obj),
+};
